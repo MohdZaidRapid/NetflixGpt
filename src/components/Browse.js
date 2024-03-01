c9922464ccc2af0cdf2d8508e55ae9e7876e41f4
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-02 00:23:11 +0530

    added movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlaying";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 // import { API_OPTIONS } from "../utils/constants";
 // import { useDispatch } from "react-redux";
 // import { addNowPlayingMovies } from "../utils/moviesSlice";
@@ -11,6 +13,19 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/**
+         MainContainer
+            - VideoBackground
+            - VideoTitle
+        Secondary Container
+          - MovisList * n
+          - Cards * n     
+        
+          
+
+         */}
     </div>
   );
 };
