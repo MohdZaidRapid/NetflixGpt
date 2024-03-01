6e82a598d304a857180c5bd4232cdac54ce42cc7
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-03-01 23:48:10 +0530

    added movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,21 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { json } from "react-router-dom";
+import useNowPlayingMovies from "../hooks/useNowPlaying";
+// import { API_OPTIONS } from "../utils/constants";
+// import { useDispatch } from "react-redux";
+// import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json.results);
-  };
+  useNowPlayingMovies();
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
   return (
     <div>
       <Header />
